fe9bcc536358ed229a131dc086e9c0c70114e49f
Marc Riesen <marcriesen@marcs-macbook.home>
2016-07-09 16:05:49 +0200

    landingpage erstellt und gestylet

---
+++ landingpage/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>easy2park.ch</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic,400italic,500,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+<div class="container-logo">
+    <div class="logo"><a id="logo" href="http://www.easy2park.ch" target="_blank"></a></div>
+</div>
+
+<div class="container-content">
+    <div class="content">
+        <h2>
+            Stell' dir vor
+            Bald kannst du bargel- und tickelos in der <br>
+            ganzen Schweiz parkieren...
+        </h2>
+        <h4>
+            Haltet mich bitte auf dem Laufenden
+        </h4>
+        <input type="email" name="" placeholder=" E-Mail Adress eingeben"> <button>ABSENDEN</button>
+    </div>
+</div>
+
+
+
+
+
+
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-57738310-2', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
