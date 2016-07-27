4f954d6fb70ac3c7f924c704f9f5d9af313d51c4
Marc Riesen <marcriesen@marcs-macbook.home>
2016-07-27 21:28:36 +0200

    landingpage html createt and styled, first js incuded

---
+++ appdev/parking/js/functions.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+    $(function() {
+        var $select = $("1-100");
+        for (var i = 1; i <= 300; i++) {
+            $select.append($('<option></option>').val(i).html(i))
+        }
+    })
+});
+
