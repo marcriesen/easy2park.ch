caa9160b38e185ed76664092135936da92880638
Marc Riesen <marcriesen@marcs-macbook.home>
2016-08-08 17:06:39 +0200

    started with parkhausDS

---
+++ appdev/parking/js/parkhausDS.js
@@ -0,0 +1,9 @@
+easy2park.parkhausDS = function () {
+    'use strict';
+
+    return {
+        create: create,
+        update: update,
+        delete: delete,
+        list: list
+    };
---
+++ appdev/parking/parkhaus.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    'use strict';
+
+    // Backlink hinzufügen
+    var $link = $("<a/>", {href:'./index.html', text: 'zurücj zu den Parkplätzen'});
+    $("body").prepend($link);
+
+    var $parkhausliste = $('#parkhausliste');
+
+    var modus = 'work';
+
+
+
+
+
+
+
+
+
+
+
+});
