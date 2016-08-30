dc50a428283de8f1b16b04697516cf551d96cf8c
Marc Riesen <marcriesen86@gmail.com>
2016-08-30 21:08:45 +0200

    styling frontpage

--- appdev/parking/js/functions.js
+++ appdev/parking/js/functions.js
@@ -1,20 +1,16 @@
 $(document).ready(function () {
-
-
-
-
-
-
-
-
-
-
-
     $(function() {
         var $select = $("1-100");
         for (var i = 1; i <= 300; i++) {
             $select.append($('<option></option>').val(i).html(i))
         }
     })
+
+    function hideIcon(self) {
+        self.style.backgroundImage = 'none';
+    }
+
+
+
 });
 
