dd73a4fe25531263517a02e5e110fd2da2c739cb
Marc Riesen <marcriesen86@gmail.com>
2016-09-08 23:27:15 +0200

    starting bestellen.php

--- appdev/parking/uebersicht.php
+++ appdev/parking/uebersicht.php
@@ -2,20 +2,20 @@
 
 
                 <h1>Datenübersicht</h1>
-               <!-- <form method="post" action="php/api/parkhaus/list.php">
+                <form method="post" action="php/api/parkhaus/list.php">
                     <select name="parkhausName">
                         <option value="parkhaus">Parkhaus auswähle</option>
                     </select>
                     <input type="submit" value="Lade Parkhausdaten"/>
-                </form> -->
+                </form>
 
-                    <div class="liste-parkhaus">
-                        <div class="parkhausuebersicht">Parkhausübersicht</div>
-                    </div>
-                    <div class="liste-besitzer">
-                        <div class="besitzerübersicht">Besitzerübersicht</div>
-                    </div>
-                    <div class="maps-parkhaus">Google <Maps></Maps></div>
+                <div class="liste-parkhaus">
+                    <div class="parkhausuebersicht">Parkhausübersicht</div>
+                </div>
+                <div class="liste-besitzer">
+                    <div class="besitzerübersicht">Besitzerübersicht</div>
+                </div>
+                <div class="maps-parkhaus">Google Maps darstellen</div>
 
 
 
